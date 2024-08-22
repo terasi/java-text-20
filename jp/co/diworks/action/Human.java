8d32aeba558c2f9ebfa2c0a8658b31956b044a9f
terasi <hitokunn750@gmail.com>
2024-08-22 20:52:49 +0900

    java kadai 20

--- jp/co/diworks/action/Human.java
+++ jp/co/diworks/action/Human.java
@@ -6,6 +6,12 @@ public class Human {
         
         HumanName abc=new HumanName();
         System.out.println(abc.getName());
+
+        HumanAge efg=new HumanAge();
+        System.out.println(efg.getAge());
+
+        HumanAddress hij=new HumanAddress();
+        System.out.println(hij.getAddress());
     }
 
 }
